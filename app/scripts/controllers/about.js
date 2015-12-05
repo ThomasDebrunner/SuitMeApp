876f3242dd97a8e250c8d1b87e2e5355e1369b90
najiji <thomas.debrunner@hotmail.com>
2015-12-05 16:08:39 +0100

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fashionMeAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fashionMeAppApp
+ */
+angular.module('fashionMeAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fashionMeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fashionMeAppApp
+ */
+angular.module('fashionMeAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
