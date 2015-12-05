76c08631780d919e9ddba382144014d007d120d4
Andreas <andreas@andreas>
2015-12-05 16:38:03 +0100

    no error in console

--- app/scripts/controllers/finder.js
+++ app/scripts/controllers/finder.js
@@ -1,6 +1,17 @@
 'use strict';
 
-angular.module('suitMeApp').controller('finderController', ['$http', function($http){
+angular.module('suitMeApp').controller('FinderController', ['$http', function($http){
+
+var globalvar;
+
+var updateDeleteId = function(articleId){
+    var localVar;
+
+};
+
+var testFunction = function(){
+    console.log("hi\n");
+}
 
 
-}};
+}]);
